f642996cd76d19a26149a0e1d39f23ca7cf0da65
João Victor Graciano Belfort de Andrade <belfort.joao216@gmail.com>
2023-06-29 21:01:47 -0300

    rotação

--- src/view/game_window.py
+++ src/view/game_window.py
@@ -228,11 +228,11 @@ class GameWindow:
             self.play_button.config(state=tk.DISABLED)
         elif self.duelist2.life_points <= 0:
             self.result_label.config(text=f"{self.duelist2.name} foi derrotado!", fg="red")
-            self.duelist1_img.config(image=self.duelist2_img_morto)
+            self.duelist1_img.config(image=self.duelist1_img_morto)
             self.play_button.config(state=tk.DISABLED)
         elif self.duelist1.life_points <= 0:
             self.result_label.config(text=f"{self.duelist1.name} foi derrotado!", fg="red")
-            self.duelist2_img.config(image=self.duelist1_img_morto)
+            self.duelist2_img.config(image=self.duelist2_img_morto)
             self.play_button.config(state=tk.DISABLED)
 
         self.turn += 1
