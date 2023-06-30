157252e76e762997cd4bd9520106214743299810
João Victor Graciano Belfort de Andrade <belfort.joao216@gmail.com>
2023-06-30 20:11:43 -0300

    cuool

--- src/controller/generate_AFD2.py
+++ src/controller/generate_AFD2.py
@@ -1,6 +1,9 @@
 import random
 
-
+max_states = 20
+max_cura = 10
+max_ataque = 10
+max_defesa = 10
 def generate_afd(num_states, limit_c, limit_a, limit_d, max_states, output_file):
     states = []
 
@@ -46,7 +49,61 @@ def generate_afd(num_states, limit_c, limit_a, limit_d, max_states, output_file)
             current_state, input_symbol, next_state = transition
             file.write(current_state + " -> " + next_state + " | " + input_symbol + "\n")
 
+class DueloAFD:
+    def __init__(self, vida_inicial):
+        self.duelista1 = Duelista(vida_inicial)
+        self.duelista2 = Duelista(vida_inicial)
+        self.transicoes1,self.st1,self.ini1 = self.ler_afd("D1.txt")
+        self.transicoes2,self.st2,self.ini2 = self.ler_afd("D2.txt")
 
-generate_afd(10, 5, 7, 3, 2, 'afd.txt')
+    def ler_afd(self, input_file):
+        tran = []
+        generate_afd(max_states, max_cura, max_ataque, max_defesa, max_states, input_file)
+        with open(input_file, 'r') as file:
+            for line in file:
+                line = line.strip()
+                if line.startswith('Q:'):
+                    states = line[3:].split()
+                elif line.startswith('I:'):
+                    initial_state = line[3:]
+                elif '->' in line and '|' in line:
+                    transition_info = line.split('->')
+                    current_state = transition_info[0].strip()
+                    next_state, symbol = transition_info[1].split('|')
+                    next_state = next_state.strip()
+                    symbol = symbol.strip()
+                    tran.append((current_state, symbol, next_state))
+        print(tran, states, initial_state)
+        return tran, states, initial_state
+    def turno(self, valor: str):
+        for v in valor:
+            print(v)
+
+
+class Duelista:
+    def __init__(self, vida_inicial):
+        self.vida = vida_inicial
+        self.afd = None
+
+    def executar_movimento(self, next_state):
+        if next_state.startswith('C'):
+            self.estado_atual = next_state
+            self.cura(int(next_state[1]))
+        elif next_state.startswith('A'):
+            self.estado_atual = next_state
+            self.ataque(int(next_state[1]))
+        elif next_state.startswith('D'):
+            self.estado_atual = next_state
+            self.defesa(int(next_state[1]))
+
+    def cura(self, pontos_cura):
+        self.vida += pontos_cura
+
+    def ataque(self, pontos_ataque):
+        self.vida -= pontos_ataque
+
+    def defesa(self, pontos_defesa):
+        self.vida += pontos_defesa
+
 
 
